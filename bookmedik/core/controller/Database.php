6f144533e6f9c55a21c42e07c81b9fe7978fb991
afermor8 <ara.fer.mor@gmail.com>
2023-03-08 20:29:11 +0100

    He corregido fallos

--- bookmedik/core/controller/Database.php
+++ bookmedik/core/controller/Database.php
@@ -1,24 +1,24 @@
 <?php
 class Database {
-    public static $db;
-    public static $con;
-    function Database(){
-            $this->user=getenv('BOOKMEDIK_DB_USER');$this->pass=getenv('BOOKMEDIK_DB_PASSWORD');$this->host=getenv('BOOKMEDIK_DB_HOST');$this->ddbb=getenv('BOOKMEDIK_DB_NAME');
-    }
+	public static $db;
+	public static $con;
+	function Database(){
+		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bookmedik";
+	}
 
-    function connect(){
-            $con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
-            $con->query("set sql_mode=''");
-            return $con;
-    }
+	function connect(){
+		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
+		$con->query("set sql_mode=''");
+		return $con;
+	}
 
-    public static function getCon(){
-            if(self::$con==null && self::$db==null){
-                        self::$db = new Database();
-                        self::$con = self::$db->connect();
-            }
-            return self::$con;
-    }
-
+	public static function getCon(){
+		if(self::$con==null && self::$db==null){
+			self::$db = new Database();
+			self::$con = self::$db->connect();
+		}
+		return self::$con;
+	}
+	
 }
 ?>
